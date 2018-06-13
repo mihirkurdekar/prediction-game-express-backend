11ff688b176ffae7a0ea6761abf7936ff1392bac
Mihir <mihir.kurdekar@gmail.com>
2018-06-14 00:28:49 +0530

    trying to fix multiple connections.

--- app/connection-config.ts
+++ app/connection-config.ts
@@ -1,12 +1,20 @@
 import mysql from 'mysql';
 
 export class Connection {
+
+    private static connection: mysql.Connection;
+
     static getConnection(): mysql.Connection {
-        return mysql.createConnection({
-            host: '',
-            user: '',
-            password: '',
-            database: ''
-        });
+        if (this.connection && this.connection != null) {
+
+        } else {
+            this.connection = mysql.createConnection({
+                host: '',
+                user: '',
+                password: '',
+                database: 'worldcup'
+            });
+        }
+        return this.connection;
     }
 }
